64ac0e38bea30c39810942684e036fd3367777ef
000wode <2943587970@qq.com>
2026-07-25 23:41:37 +0800

    Add files via upload

---
+++ day01_hello.py
@@ -0,0 +1,12 @@
+# 弥天的第一行 Python 代码
+print("Hello, 弥天！今天开始 45 天蜕变计划。")
+
+# 变量练习
+name = "弥天"
+days = 45
+hours_per_day = 4.5
+total_hours = days * hours_per_day
+
+print(f"{name} 将在 {days} 天内投入 {total_hours} 小时")
+
+# 猜猜输出是什么？
---
+++ day02_condition.py
@@ -0,0 +1,10 @@
+score = 95
+
+if score >= 90:
+    print("优秀")
+elif score >= 80:
+    print("良好")
+elif score >= 60:
+    print("及格")
+else:
+    print("不及格")
